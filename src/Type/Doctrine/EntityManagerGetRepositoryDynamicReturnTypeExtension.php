d8300b9feabe022a5aa53b6cd64a22093a5f64fc
artembegood <artembegood@alterpost.org>
2018-02-22 17:01:19 +0200

    #

--- src/Type/Doctrine/EntityManagerGetRepositoryDynamicReturnTypeExtension.php
+++ src/Type/Doctrine/EntityManagerGetRepositoryDynamicReturnTypeExtension.php
@@ -10,55 +10,70 @@ use PHPStan\Type\Type;
 class EntityManagerGetRepositoryDynamicReturnTypeExtension implements \PHPStan\Type\DynamicMethodReturnTypeExtension
 {
 
-	/**
-	 * @var string
-	 */
-	private $repositoryClass;
+    /**
+     * @var array
+     */
+    private $repositoryClass;
 
-	public function __construct(string $repositoryClass)
-	{
-		$this->repositoryClass = $repositoryClass;
-	}
+    public function __construct(array $repositoryClass)
+    {
+        $this->repositoryClass = $repositoryClass;
+    }
 
-	public function getClass(): string
-	{
-		return \Doctrine\Common\Persistence\ObjectManager::class;
-	}
+    public function getClass(): string
+    {
+        return \Doctrine\Common\Persistence\ObjectManager::class;
+    }
 
-	public function isMethodSupported(MethodReflection $methodReflection): bool
-	{
-		return $methodReflection->getName() === 'getRepository';
-	}
+    public function isMethodSupported(MethodReflection $methodReflection): bool
+    {
+        return $methodReflection->getName() === 'getRepository';
+    }
 
-	public function getTypeFromMethodCall(
-		MethodReflection $methodReflection,
-		MethodCall $methodCall,
-		Scope $scope
-	): Type
-	{
-		if (count($methodCall->args) === 0) {
-			return $methodReflection->getReturnType();
-		}
-		$arg = $methodCall->args[0]->value;
-		if (!($arg instanceof \PhpParser\Node\Expr\ClassConstFetch)) {
-			return $methodReflection->getReturnType();
-		}
+    public function getTypeFromMethodCall(
+        MethodReflection $methodReflection,
+        MethodCall $methodCall,
+        Scope $scope
+    ): Type
+    {
+        if (count($methodCall->args) === 0) {
+            return $methodReflection->getReturnType();
+        }
+        $arg = $methodCall->args[0]->value;
+        if (!($arg instanceof \PhpParser\Node\Expr\ClassConstFetch)) {
+            return $methodReflection->getReturnType();
+        }
 
-		$class = $arg->class;
-		if (!($class instanceof \PhpParser\Node\Name)) {
-			return $methodReflection->getReturnType();
-		}
+        $class = $arg->class;
+        if (!($class instanceof \PhpParser\Node\Name)) {
+            return $methodReflection->getReturnType();
+        }
 
-		$class = (string) $class;
-		if ($class === 'static') {
-			return $methodReflection->getReturnType();
-		}
+        $class = (string) $class;
+        if ($class === 'static') {
+            return $methodReflection->getReturnType();
+        }
 
-		if ($class === 'self') {
-			$class = $scope->getClassReflection()->getName();
-		}
+        if ($class === 'self') {
+            $class = $scope->getClassReflection()->getName();
+        }
 
-		return new EntityRepositoryType($class, $this->repositoryClass);
-	}
+        $className = substr($class, strrpos($class, '\\') + 1);
+        $targetRepositoryClass = '';
+
+        // find corresponding repository class by entity class name
+        foreach ($this->repositoryClass as $repositoryClass){
+            if (strpos($repositoryClass, $className)) {
+                $targetRepositoryClass = $repositoryClass;
+                break;
+            }
+        }
+
+        if (empty($targetRepositoryClass)) {
+            return $methodReflection->getReturnType();
+        }
+
+        return new EntityRepositoryType($class, $targetRepositoryClass);
+    }
 
 }
